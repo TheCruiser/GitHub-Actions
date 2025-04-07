c9d948759aa4497eedc5faeb783532c2e998eacf
Satish Ranjan Rout <satishranjanrout10395@gmail.com>
2025-04-07 06:25:01 +0530

    update

--- src/basic.py
+++ src/basic.py
@@ -2,6 +2,6 @@ def powerof2(a):
     return a**2
     
 def test_powerof2():
-    assert powerof2(2,2) == 4
-    assert powerof2(3,2) == 9
-    assert powerof2(4,2) == 16
+    assert powerof2(2) == 4
+    assert powerof2(3) == 9
+    assert powerof2(4) == 16
